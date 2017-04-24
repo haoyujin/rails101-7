8bdbe431f447721304015e7a762cf4926a39f778
haoyujin <haoyujin2012@gmail.com>
2017-04-24 20:02:00 +0800

    implement account/posts#Edit&Delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+    @post.update(post_params)
+    redirect_to account_posts_path, notice: "[@post]修改成功！"
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "已经删除！"
+  end
+
   private
 
   def post_params
